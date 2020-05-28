79b4912bbfff7d616922a503f46b171616766bb2
yuizho <yui-ito@bm-sms.co.jp>
2020-05-28 22:27:31 +0900

    modify tabula_sample to read with area

--- tabula_sample.py
+++ tabula_sample.py
@@ -1,11 +1,34 @@
 import tabula
 
 
+def extranct(saurce_row):
+    return list(filter(
+        lambda x: len(x) > 0,
+        [[r for r in row if r['top'] > 103] for row in saurce_row]
+        ))
+
+
 df = tabula.read_pdf(
     "service_code.pdf",
-    guess=True,
+    # guess=True,
+    area=[
+        [63.89, 38.12, 63.89+759.38, 38.12+145.69],
+        [63.89, 498.24, 63.89+759.38, 498.24+61.31]
+    ],
     lattice=True,
+    multiple_tables=False,
     format="JSON",
     pages="3"
 )
-print(df)
+
+joined = []
+for service, unit in zip(extranct(df[0]['data']), extranct(df[1]['data'])):
+    joined.append([s['text'] for s in service] + [u['text'] for u in unit])
+
+how = ""
+for j in joined:
+    if len(j) >= 5:
+        how = j[4]
+    else:
+        j.append(how)
+print(joined)
